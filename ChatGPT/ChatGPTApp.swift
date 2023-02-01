4be8f602fba82b14b167bb5911df13d221b156e9
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-01 22:45:31 +0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Abdullah Karaboğa on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
